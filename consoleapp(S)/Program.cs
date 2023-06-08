2299d21401e7b972fe3cadc5d03457ac182bf35f
hp <hp@DESKTOP-2VE9LHI>
2023-06-08 18:58:29 +0400

    Add project files.

---
+++ consoleapp(S)/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Diagnostics.SymbolStore;
+
+Console.WriteLine("Enter Firs number");
+double number1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter Second number");
+double number2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter Simbol operation");
+char symbol = char.Parse(Console.ReadLine());
+
+if (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/' || symbol == '%') 
+{ 
+}
+
+else if (symbol == '+')
+    Console.WriteLine($"{number1 + number2}={number1 + number2}");
+else if (symbol == '-')
+    Console.WriteLine($"{number1 - number2}={number1 + number2}");
+else if (symbol == '*')
+    Console.WriteLine($"{number1 * number2}={number1 * number2}");
+else if (symbol == '/')
+    Console.WriteLine($"{number1 / number2}={number1 / number2}");
+else if (symbol == '%')
+    Console.WriteLine($"{number1 % number2}={number1 % number2}");
+
+else
+{
+    Console.WriteLine("text is not a number");
+}
+Console.ReadLine();
